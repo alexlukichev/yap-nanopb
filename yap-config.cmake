c16a331181bab9a0b86266484c00ca2da3abf48a
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2017-08-10 13:58:54 -0500

    initial commit

---
+++ yap-config.cmake
@@ -0,0 +1,15 @@
+if(NOT YAP_PACKAGE_VERSION)
+    message(FATAL_ERROR "Yap package version requirements not specified")
+endif()
+
+yap_require(git+https://github.com/nanopb/nanopb VERSION ${YAP_PACKAGE_VERSION} NAME _NANOPB)
+
+set(NANOPB_SRC_ROOT_FOLDER "${_NANOPB_SOURCE}")
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${NANOPB_SRC_ROOT_FOLDER}/extra)
+find_package(Nanopb REQUIRED)
+if(Nanopb_NOT_FOUND)
+  message(SEND_ERROR "Error: nanopb not found")
+  return()
+endif()
+
+set(${YAP_PACKAGE_NAME}_INCLUDE_DIRECTORIES ${NANOPB_INCLUDE_DIRS} PARENT_SCOPE)
