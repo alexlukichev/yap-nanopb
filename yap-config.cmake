3b49f1843b79191f1fa8d977830c30d69fdc130c
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2017-08-10 19:10:11 -0500

    don't import all targets from nanpb

--- yap-config.cmake
+++ yap-config.cmake
@@ -2,7 +2,7 @@ if(NOT YAP_PACKAGE_VERSION)
     message(FATAL_ERROR "Yap package version requirements not specified")
 endif()
 
-yap_require(git+https://github.com/nanopb/nanopb VERSION ${YAP_PACKAGE_VERSION} NAME _NANOPB)
+yap_require(git+https://github.com/nanopb/nanopb VERSION ${YAP_PACKAGE_VERSION} NAME _NANOPB EXCLUDE_FROM_ALL)
 
 set(NANOPB_SRC_ROOT_FOLDER "${_NANOPB_SOURCE}")
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${NANOPB_SRC_ROOT_FOLDER}/extra)
